91282bd9868019c741914ec2524d3f323db769be
mateus-brites <mateusmmo15@gmail.com>
2022-01-28 06:23:37 -0300

    criei meu primeiro estado

---
+++ ChapterI/src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+export function COunter() {
+
+    const [counter, setCounter] = useState(0);
+    function increment() {
+        setCounter(counter + 2)
+    }
+
+    return (
+        <div>
+            <h2>{counter}</h2>
+
+            <button type="button" onClick={increment}>increment</button>
+        </div>
+    )
+}
