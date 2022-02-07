5897e20de978bfc1a4226ea30f94166c8a3238c1
mateus-brites <mateusmmo15@gmail.com>
2022-02-07 00:37:25 -0300

    corrigi alguns erros

--- ChapterII/dtmoney/src/components/TransactionsTables/styles.ts
+++ ChapterII/dtmoney/src/components/TransactionsTables/styles.ts
@@ -1,38 +1,33 @@
-import styled from "styled-components";
-
+import styled from 'styled-components';
 
 export const Container = styled.div`
-    border: 1px solid red;
-
-    margin-top: 4rem;
-
-    table {
-        width: 100%; // Para tabela ocupar toda a largura
-        border-spacing: 0 0.5rem; // Para fazer espaçamento
-    }
-    
+  margin-top: 4rem;
+  
+  table {
+    width: 100%; // Para tabela ocupar toda a largura
+    border-spacing: 0 0.5rem; // espaçamento
     th {
-        color: var(--text-body);
-        font-weight: 600;
-        padding: 1rem 2rem;
+      color: var(--text-body);
+      font-weight: 400;
+      padding: 1rem 2rem;
+      text-align: left;
+      line-height: 1.5rem;
     }
     td {
-        padding: 1rem 2rem;
-        border: 1px solid red;
-        background: var(--shape);
-        color: var(--text-body);
-        border-radius: 0.25rem; // arredonda as bordas
-
-        &.first-child {
-            color: var(--text-title);
-        }
-
-        &.deposit {
-            color: var(--green);
-        }
-
-        &.withdraw {
-            color: var(--red);
-        }
+      padding: 1rem 2rem;
+      border: 0;
+      background: var(--shape);
+      color: var(--text-body);
+      border-radius: 0.25rem; // arredonda as bordas
+      &:first-child {
+        color: var(--text-title);
+      }
+      &.deposit {
+        color: var(--green);
+      }
+      &.withdraw {
+        color: var(--red);
+      }
     }
-`
+  }
+`;
