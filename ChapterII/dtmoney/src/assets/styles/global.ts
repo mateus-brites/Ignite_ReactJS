eaeb2167710783660843021fab0f46cc62569239
mateus-brites <mateusmmo15@gmail.com>
2022-02-08 14:26:44 -0300

    estilizando o modal

--- ChapterII/dtmoney/src/assets/styles/global.ts
+++ ChapterII/dtmoney/src/assets/styles/global.ts
@@ -76,4 +76,17 @@ export const GlobalStyles = createGlobalStyle`
         border-radius: 0.25rem;
     }
 
+    .react-modal-close {
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        background: transparent;
+
+        transition: 0.2s;
+        &:hover {
+            filter: brightness(0.5);
+        }
+    }
+
 `
