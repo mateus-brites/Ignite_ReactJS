587b446ef435cc46f91ecccaca51252c37c1013c
mateus-brites <mateusmmo15@gmail.com>
2022-02-12 16:09:28 -0300

    listando transaçoes

--- ChapterII/dtmoney/src/components/TransactionsTables/index.tsx
+++ ChapterII/dtmoney/src/components/TransactionsTables/index.tsx
@@ -1,12 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+    id: number;
+    title: string;
+    amount: number;
+    type: string;
+    category: string;
+    createdAt: string;
+}
 
 export function TransactionsTables() {
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
     useEffect(() => {
         api.get('transactions')
-            .then(Response => console.log(Response.data))
+            .then(Response => setTransactions(Response.data.transactions))
     }, [])
     return (
         <Container>
@@ -21,18 +31,14 @@ export function TransactionsTables() {
                 </thead>
 
                 <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw"> - R$1.200</td>
-                        <td>casa</td>
-                        <td>20/03/2021</td>
-                    </tr>
+                    {transactions.map(transaction => ( // Toda vez que usamos o map, o primeiro elemento precisa ter uma key informando qual o elemento único do objeto
+                        <tr key={transaction.id}>
+                            <td>{transaction.title}</td>
+                            <td className={transaction.type}>R${transaction.amount}</td>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.createdAt}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </Container>
--- ChapterII/dtmoney/src/index.tsx
+++ ChapterII/dtmoney/src/index.tsx
@@ -7,6 +7,29 @@ createServer({
   models: { // iniciando um repositorio fake no banco fake.
     transaction: Model,
   },
+  
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'casa',
+          amount: 806,
+          createdAt: new Date('2021-02-14 14:00:00')
+        }
+      ]
+    })
+  },
 
   routes() {
     this.namespace = 'api';
