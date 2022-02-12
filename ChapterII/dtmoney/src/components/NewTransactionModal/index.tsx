cefd9d81d02eb97be17b7971441121b4157707f5
mateus-brites <mateusmmo15@gmail.com>
2022-02-12 15:39:53 -0300

    inserindo transaçao na api

--- ChapterII/dtmoney/src/components/NewTransactionModal/index.tsx
+++ ChapterII/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { api } from '../../services/api'
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 interface NewTransactionModalProps {
@@ -10,12 +11,41 @@ interface NewTransactionModalProps {
     onRequestClore: () => void;
 }
 
+
+
+
+
 export function NewTransactionModal({
     isOpen: isNewTransactionModalOpen,
     onRequestClore: handleCloseNewTransactionModal
     }: NewTransactionModalProps) {
 
+        const [title, setTitle] = useState('');
+        const [value, setValue] = useState(0);
+        const [category, setCategory] = useState('');
         const [type, setType] = useState('deposit');
+
+        function handleCreateNewTransaction(event: FormEvent) {
+            event.preventDefault() // Previne que a página seja recarregada ao enviar um formulário.
+            const data = {
+                title,
+                value,
+                category,
+                type,
+            }
+
+            api.post('/transactions', data)
+
+            handleCloseNewTransactionAndResetForm()
+        }
+        
+
+        function handleCloseNewTransactionAndResetForm() {
+            handleCloseNewTransactionModal()
+            setTitle('')
+            setCategory('')
+            setValue(0)
+        }
         
         return (
             
@@ -29,10 +59,19 @@ export function NewTransactionModal({
             type='button' onClick={handleCloseNewTransactionModal} className='react-modal-close'>
                 <img src={closeImg} alt='fechar modal' />
             </button>
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
-                <input placeholder='titulo'/>
-                <input placeholder='valor' type='number'/>
+                <input
+                    placeholder='titulo'
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
+                />
+                <input
+                    placeholder='valor'
+                    type='number'
+                    value={value}
+                    onChange={event => setValue(Number(event.target.value))}
+                />
 
                 <TransactionTypeContainer>
                     <RadioBox
@@ -55,7 +94,11 @@ export function NewTransactionModal({
                     </RadioBox>
 
                 </TransactionTypeContainer>
-                <input placeholder='categoria'/>
+                <input
+                    placeholder='categoria'
+                    value={category}
+                    onChange={event => setCategory(event.target.value)}    
+                />
 
                 <button type='submit'>Cadastrar</button>
             </Container>
