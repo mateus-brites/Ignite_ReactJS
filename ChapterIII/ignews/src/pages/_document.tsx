8f140b637894b45e7d0bd4b8e0adb45364011b0c
mateus-brites <mateusmmo15@gmail.com>
2022-03-30 23:43:53 -0300

    adicionando fontes externas

---
+++ ChapterIII/ignews/src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, {Html, Head, Main, NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+                    <title>Ignews</title>
+                </Head>
+                <body>
+                    <Main />  {/* todo conteudo da aplicação será renderizado nessa main */}
+                    <NextScript />  {/* Onde o next colocará os arquivos js necessários para aplicação functionar */}
+                </body>
+            </Html>
+        )   
+    }
+}
