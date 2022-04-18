c6f4be3040b37c1994bb37633df51079a8e236bb
mateus-brites <mateusmmo15@gmail.com>
2022-04-18 20:37:14 -0300

    title dinamico por pagina

--- ChapterIII/ignews/src/pages/index.tsx
+++ ChapterIII/ignews/src/pages/index.tsx
@@ -1,7 +1,13 @@
-import style from './styles/home.module.scss'
+import Head from 'next/head'
+import style from '../styles/home.module.scss'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Ignews</title>
+    </Head>
     <h1 className={style.title}>Hello World</h1>
+    </>
   )
 }
