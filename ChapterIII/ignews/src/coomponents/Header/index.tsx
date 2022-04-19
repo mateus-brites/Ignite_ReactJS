5f03d18be1cfd06800027e6925b7a53668c19a72
mateus-brites <mateusmmo15@gmail.com>
2022-04-19 08:48:05 -0300

    m

---
+++ ChapterIII/ignews/src/coomponents/Header/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss';
+
+export function Header() {
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news"/>
+                <nav>
+                {/* <nav> menu de navegação */}
+                    <a  className={styles.active}>Homes</a>
+                    <a>Posts</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
