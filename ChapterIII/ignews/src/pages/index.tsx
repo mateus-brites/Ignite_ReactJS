e7bbc4233c2a4ad6ec4d6c658a4d8a5a8553ec20
Mateus Brites <mateusmmo15@gmail.com>
2022-05-21 23:32:53 -0300

    consumindo API do Stripe

--- ChapterIII/ignews/src/pages/index.tsx
+++ ChapterIII/ignews/src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../coomponents/SubscribeButton';
+import { stripe } from '../services/stripe';
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProduct {
+  product:{
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home(props: HomeProduct) {
+  const product = props.product
   return (
     <>
     <Head>
@@ -14,13 +24,29 @@ export default function Home() {
         <h1>News about the <span>React</span> world</h1>
         <p>
           Get acess to all the publication <br />
-          <span>for $9.98 monthly</span>
+          <span>for {product.amount} monthly</span>
         </p>
-        <SubscribeButton />
+        <SubscribeButton priceId={product.priceId} />
 
       </section>
       <img src='/images/avatar.svg' alt='girls coding' />
     </main>
     </>
   )
+}
+// tudo que retornarmos de props aqui poderemos utilizar dentro do componente Home compo propriedade
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1L238OIhkTTwHkXI1I4ivRei'); // API ID
+  expend: ['product'] // para ter acesso a todas as informações do produto
+
+  
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return ({ props: { product, } })
 }
---
+++ ChapterIII/ignews/src/services/stripe.ts
@@ -0,0 +1,13 @@
+import Stripe from 'stripe';
+
+export const stripe = new Stripe(
+    process.env.API_SECRET_KEY,
+    {
+        apiVersion: '2020-08-27',
+        appInfo: {
+            name: 'Ignews',
+            version: '0.1.8'
+        }
+        
+    }
+)
