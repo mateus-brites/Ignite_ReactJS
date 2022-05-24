923fd53953533d90c66d4a5b8e2a20830823d82e
Mateus Brites <mateusmmo15@gmail.com>
2022-05-24 01:12:19 -0300

    autenticaçao com next auth

--- ChapterIII/ignews/src/coomponents/SingInButton/index.tsx
+++ ChapterIII/ignews/src/coomponents/SingInButton/index.tsx
@@ -1,16 +1,20 @@
 import { FaGithub} from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss';
+import { signIn, useSession, signOut } from "next-auth/react"
 
+// dentro de signIn devemos informar qual tipo de autenticação iremos utilizar naquele botão
+// useSession retorna informações se o usuário está em um sessão ativa
 export function SignInButton() {
-    const isUserLogIn = true
-    return isUserLogIn ? (
-    <button type="button" className={styles.signInButton}>
+    const { data } = useSession() // retorna informações do usuário e informa se ele está ativo ou não
+    console.log(data)
+    return data ? (
+    <button type="button" className={styles.signInButton} onClick={() => signOut()}>
         <FaGithub color='#04b361' />
-        Sign in with Github
+        {data.user.name}
         <FiX color='#737380' className={styles.closeIcon} />
     </button>) : (
-        <button type="button" className={styles.signInButton}>
+        <button type="button" className={styles.signInButton} onClick={() => signIn('github')}>
         <FaGithub color='#eba417' />
         Sign in with Github
         <FiX color='#737380' className={styles.closeIcon} />
--- ChapterIII/ignews/src/pages/_app.tsx
+++ ChapterIII/ignews/src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from 'next/app'
 import { Header } from '../coomponents/Header'
+import { SessionProvider as NextAuthProvider } from "next-auth/react"
 
 import '../styles/global.scss'
 
+// Ao recarregar a pagina as informações se o usuário está logado ou não chegam através do pageProps.session
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-    <Header />
-    <Component {...pageProps} />
-    </>
+    <NextAuthProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </NextAuthProvider>
   )
 }
 
