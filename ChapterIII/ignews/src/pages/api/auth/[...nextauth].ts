1a33adae0833193c7a72f97f1567f7c2bb949078
Mateus Brites <mateusmmo15@gmail.com>
2022-05-24 22:49:25 -0300

    usando fauna db

--- ChapterIII/ignews/src/pages/api/auth/[...nextauth].ts
+++ ChapterIII/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -14,4 +19,42 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({user, account, profile}): Promise<boolean> {
+
+      const { email } = user
+      console.log(user)
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'), // esse indice foi criado dentro do fauna
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'), // Nome da tabela que queremos inserir
+              {
+                data: { email: email }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+  
+        return true
+      } catch {
+        return false
+      }
+    }
+  },
 })
