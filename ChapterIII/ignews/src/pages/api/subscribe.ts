560d8796a23c612e2e13801029deed31e978465a
Mateus Brites <mateusmmo15@gmail.com>
2022-05-30 23:42:21 -0300

    evitando usuarios duplicados

--- ChapterIII/ignews/src/pages/api/subscribe.ts
+++ ChapterIII/ignews/src/pages/api/subscribe.ts
@@ -1,17 +1,57 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse} from 'next'
 import { getSession } from 'next-auth/react'
+import { fauna } from '../../services/fauna';
+import { query as q } from 'faunadb'
 import { stripe } from '../../services/stripe';
+
+type User = {
+    ref: {
+        id: string
+    };
+
+    data: {
+        strip_customer_id: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if(req.method = 'POST') {
         const session = await getSession({ req });
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
-            // metadata
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(session.user.email)
+                )
+            )
+        )
+
+        let customerId = user.data.strip_customer_id
+
+        if(!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+                // metadata
+            })
+    
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id), {
+                        data: {
+                            strip_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id
+
+        }
+
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required', // diz se quer obrigar o usuário a preencher o endereço ou não
             line_items: [{ price: 'price_1L238OIhkTTwHkXI1I4ivRei', quantity: 1}],
