055a68cbccfab018087fd8a3cc7af643724ebb2f
Mateus Brites <mateusmmo15@gmail.com>
2022-06-12 02:30:36 -0300

    finalizando Header

--- ChapterIV/dashgo/src/components/Header.tsx
+++ ChapterIV/dashgo/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Icon, Input, HStack, Text, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -51,6 +51,43 @@ export function Header() {
                 _placeholder={{ color: 'gray.400'}}
                 />
                 <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                {/*Mesma coisa o Stack porém mantem os itens ao lado um do outro */}
+                <HStack 
+                    spacing="4"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                > 
+                    <Icon as={RiNotificationLine} fontSize="28" type="button" />
+                    <Icon as={RiUserAddLine} fontSize="28" type="button" _hover={{color: 'yellow'}}/>
+                </HStack>
+
+                <Flex alignItems="center">
+                    <Box
+                        mr="4"
+                        textAlign="right">
+                        <Text
+                        >
+                            Nome do usuário
+                        </Text>
+                        <Text
+                            color="gray.300"
+                            fontFamily="small"
+                        >
+                            Email do usuário
+                        </Text>
+                    </Box>
+                    <Avatar size="md" name='Mateus Brites' src='' />
+                </Flex>
             </Flex>
         </Flex>
     )
