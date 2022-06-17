8d13ad676bd9344581e27d4a0ac8d91a2017eb6d
Mateus Brites <mateusmmo15@gmail.com>
2022-06-17 00:07:58 -0300

    forms de validaçao

--- ChapterIV/dashgo/src/components/Form/Input.tsx
+++ ChapterIV/dashgo/src/components/Form/Input.tsx
@@ -1,11 +1,12 @@
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface InputProps extends ChakraInputProps {
     name: string;
-    label?: string;
+    label?: string; 
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
+const  InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = ({ name, label, ...rest }, ref) => {
     return(
         <FormControl> {/* Define espaçamento entre os elementos */}
             { !!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
@@ -19,8 +20,12 @@ export function Input({ name, label, ...rest }: InputProps) {
                 bgColor: 'gray.900'
               }}
               size='lg'
+              ref={ref}
               {...rest}
             />
           </FormControl>
     )
 }
+
+// Faz um encaminhamento da nossa ref, ou seja 
+export const Input = forwardRef(InputBase)
--- ChapterIV/dashgo/src/pages/index.tsx
+++ ChapterIV/dashgo/src/pages/index.tsx
@@ -1,7 +1,19 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
+import { useForm } from 'react-hook-form'
+
+interface SignInFormData {
+  email: string;
+  password: string
+}
 
 export default function SignIn() {
+  const { register, handleSubmit } = useForm()
+
+  function handleSignIn(values: SignInFormData) {
+    console.log(values)
+  }
+
   return (
     <Flex w='100vm' h='100vh' align='center' justify='center'>
       <Flex 
@@ -12,10 +24,11 @@ export default function SignIn() {
         p='8'
         borderRadius={8}
         flexDirection='column'
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">  {/* Define espaçamento entre os elementos */}
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input name="email" type="email" label="E-mail" {...register('email')}/>
+          <Input name="password" type="password" label="Senha" {...register('password')}/>
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size='lg'>Entrar</Button>
